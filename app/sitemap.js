219d238d22d941489d85b26ff9ddc8fc2abb07ba
LAZGHEM othmane <lazotman@gmail.com>
2026-04-14 02:28:19 +0200

    feat: add google verification

---
+++ app/sitemap.js
@@ -0,0 +1,22 @@
+export default function sitemap() {
+  return [
+    {
+      url: 'https://cine-max.live',
+      lastModified: new Date(),
+      changeFrequency: 'daily',
+      priority: 1,
+    },
+    {
+      url: 'https://cine-max.live/movies',
+      lastModified: new Date(),
+      changeFrequency: 'daily',
+      priority: 0.8,
+    },
+    {
+      url: 'https://cine-max.live/tv',
+      lastModified: new Date(),
+      changeFrequency: 'daily',
+      priority: 0.8,
+    },
+  ]
+}
