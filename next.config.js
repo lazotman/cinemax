608d41f8caf3e41d787d75a56f11877af89f3488
LAZGHEM othmane <lazotman@gmail.com>
2026-04-17 23:48:54 +0200

    feat: add analytics and speed insights

--- next.config.js
+++ next.config.js
@@ -1,20 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   images: {
-    remotePatterns: [
-      { protocol: 'https', hostname: 'image.tmdb.org' },
-    ],
-  },
-  async headers() {
-    return [
-      {
-        source: '/(.*)',
-        headers: [
-          { key: 'X-DNS-Prefetch-Control', value: 'on' },
-          { key: 'Cache-Control', value: 'public, max-age=3600, stale-while-revalidate=86400' },
-        ],
-      },
-    ];
+    unoptimized: true,
   },
 };
 
