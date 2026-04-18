1bcf9c88a3857778741f0d576e549cdf7be2c05b
LAZGHEM othmane <lazotman@gmail.com>
2026-04-18 20:43:32 +0200

    add movie and tv pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      { hostname: 'image.tmdb.org' },
+    ],
+  },
 };
 
 export default nextConfig;
